1e2ec224298bcf31102bd0ae45aa5c185c0d080e
SamuelGitau_School <samuel.gitau4@student.moringaschool.com>
2026-08-03 13:53:26 +0300

    done

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,24 @@
-// Code PasswordInput Component Here
+import { useState } from "react";
 
 function PasswordInput (){
+    const [password,setPassword] = useState('')
+
+    const handleChange =()=>{
+        console.log("Entering password...")
+    }
+
+    const Passwordchange = (event) =>{    
+        setPassword(event.target.value);
+        console.log('Current password:',event.target.value)
+    };
+
     return(
-        <></>
+        <>
+        <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" value={password} onChange ={Passwordchange} />
+        </div>
+        </>
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,28 @@
-// Code SubmitButton Component Here
+import { useState } from "react";
 
 function SubmitButton (){
-    return(
-        <></>
+
+    const [isHoverd,setIshovered] = useState(false);
+
+    const handleMouseEnter = ()=>{
+        setIshovered(true);
+        console.log('Mouse entered button');
+    }
+
+    const handleMouseLeave = ()=>{
+        setIshovered(false)
+        console.log('Mouse left button');
+    }
+        return(
+        <>
+        <button
+        type="submit"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        >
+            {isHoverd ? 'Ready to Submit' : 'Submit'}
+        </button>
+        </>
     )
 }
 
